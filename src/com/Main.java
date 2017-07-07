68278399b4e839ef1ffa467f142bec8b3e4cd228
Danny Luu <hisoka365@gmail.com>
2017-07-07 19:56:15 -0400

    Create permutation method

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,43 +1,24 @@
 package com;
 
-import com.trees.BinarySearchTree;
-import com.trees.BinarySearchTree.Node;
+import com.algorithms.ArraysAndStringsAlgorithms;
+import com.datastructures.trees.BinarySearchTree;
+import com.datastructures.trees.BinarySearchTree.Node;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
+        ArraysAndStringsAlgorithms arraysAndStringsAlgorithms = new ArraysAndStringsAlgorithms();
 
-        bst.insert(10);
-        bst.insert(5);
-        bst.insert(15);
-        bst.insert(7);
-        bst.insert(6);
-        bst.insert(11);
-        bst.insert(13);
-        Node root = bst.getRoot();
-        System.out.println(" 0 - left, root, right = " + root.left + "," + root + "," + root.right);
+        String word = "abcabc";
+        Set<String> permuations = arraysAndStringsAlgorithms.generatePermutations(word);
 
-        Node left = root.left;
-        System.out.println(" 1 - left, root, right = " + left.left + "," + left + "," + left.right);
+        for (String s : permuations)
+            System.out.println(s);
 
-        Node right = root.right;
-        System.out.println(" 1 - left, root, right = " + right.left + "," + right + "," + right.right);
-
-
-        Node leftRight = left.right;
-        System.out.println(" 2 - left, root, right = " + leftRight.left + "," + leftRight + "," + leftRight.right);
-
-        Node rightLeft = right.left;
-        System.out.println(" 2 - left, root, right = " + rightLeft.left + "," + rightLeft + "," + rightLeft.right);
-
-        System.out.println("parent of 11 should be 15 = " + rightLeft.parent);
-        System.out.println("parent of 5 should be 10 = " + left.parent);
-        System.out.println("parent of 15 should be 10 = " + right.parent);
-        System.out.println("parent of root should be null = " + root.parent);
-
-
-        System.out.println("Find 13 = " + bst.search(13));
-
+        System.out.println("permutation size = " + permuations.size());
     }
 }
--- src/com/algorithms/ArraysAndStringsAlgorithms.java
+++ src/com/algorithms/ArraysAndStringsAlgorithms.java
@@ -1,5 +1,10 @@
 package com.algorithms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Hisoka on 2017-07-07.
  */
@@ -10,11 +15,73 @@ public class ArraysAndStringsAlgorithms {
      * <p>
      * What if you cannot use additional data structures?
      *
-     * @param s - the string to test.
+     * @param word - the string to test.
      * @return
      */
-    public boolean isUnique(String s) {
-        System.out.println(s);
+    public boolean isUnique(String word) {
+        String currentWord = word;
+        while (currentWord.length() > 0) {
+            String[] split = currentWord.split("" + currentWord.charAt(0));
+
+            if ((split.length > 2) || (split.length == 2 && split[1].length() < currentWord.length() - 1)) {
+                return false;
+            }
+
+            if (split.length == 2) {
+                currentWord = split[1];
+            } else if (split.length == 0 && currentWord.length() == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         return true;
+    }
+
+
+    /**
+     * Given two strings, write a method to decide if one is a permutation of the other.
+     *
+     * @param permutation - potential permutation in word.
+     * @param word        - String being checked.
+     * @return
+     */
+    public boolean checkPermutation(String permutation, String word) {
+        return false;
+    }
+
+    /**
+     * Generate all of the permutations for the given string.
+     *
+     * @param input
+     * @return all permutations
+     */
+    public Set<String> generatePermutations(String input) {
+        Set<String> permutations = new HashSet<>();
+
+        if (input.length() == 1) {
+            permutations.add(input);
+        } else {
+            String permutation = input;
+            for (int i = 0; i < input.length(); i++) {
+                permutation = rotate(permutation);
+
+                Set<String> subPerm = generatePermutations(permutation.substring(1, permutation.length()));
+                for (String p : subPerm) {
+                    permutations.add(permutation.charAt(0) + p);
+                }
+            }
+        }
+
+        return permutations;
+    }
+
+    private String rotate(String input) {
+        if (input.length() <= 1) {
+            return input;
+        } else {
+            return input.substring(1, input.length()) + input.charAt(0);
+        }
     }
 }
